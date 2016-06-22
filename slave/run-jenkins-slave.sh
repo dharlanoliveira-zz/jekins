44a253809827f622e131a94d7291f929d4c6a381
Dharlan Oliveira <dharlanoliveira@gmail.com>
2016-06-22 17:00:39 -0300

    * Externalizando configuração do projeto

--- slave/run-jenkins-slave.sh
+++ slave/run-jenkins-slave.sh
@@ -2,4 +2,4 @@
 
 LABEL="slave-$(date +"%m-%d-%y_%H-%M-%S")"
 DESCRIPTION="Slave do jenkins criado dinamicamente para builds de pipelines"
-docker run -d csanchez/jenkins-swarm-slave -master http://192.168.0.10:8080 -username jenkins -password jenkins -labels $LABEL -name $LABEL -description $DESCRIPTION -executors 1
+docker run -d csanchez/jenkins-swarm-slave -master http://192.168.0.10:8080 -username jenkins -password jenkins -labels $LABEL -name $LABEL -description "$DESCRIPTION" -executors 1
