0bec54471a2672db32bf689b0789063498a6805c
Dharlan Oliveira <dharlanoliveira@gmail.com>
2016-06-22 16:07:55 -0300

    * Evoluindo

--- slave/run-jenkins-slave.sh
+++ slave/run-jenkins-slave.sh
@@ -1,2 +1,5 @@
-#!/usr/bin/env bash
-docker run -d csanchez/jenkins-swarm-slave -master http://192.168.0.10:8080 -username jenkins -password jenkins -executors 1
+#!/bin/sh
+
+LABEL="slave-$(date +"%m-%d-%y_%H-%M-%S")"
+DESCRIPTION="Slave do jenkins criado dinamicamente para builds de pipelines"
+docker run -d csanchez/jenkins-swarm-slave -master http://192.168.0.10:8080 -username jenkins -password jenkins -labels $LABEL -name $LABEL -description $DESCRIPTION -executors 1
