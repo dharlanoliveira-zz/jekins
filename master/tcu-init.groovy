454d596b3ce16b60db5d5251a0225238aafef05a
Dharlan Oliveira <dharlanoliveira@gmail.com>
2016-06-22 22:40:58 -0300

    * Externalizando configuração do projeto

--- master/tcu-init.groovy
+++ master/tcu-init.groovy
@@ -6,6 +6,20 @@ inst.disableSecurity();
 println "Desabilitando executores no master"
 inst.setNumExecutors(0);
 
+println "Configurando e-mail global"
+def config = JenkinsLocationConfiguration.get()
+config.setAdminAddress("jenkins@tcu.gov.br")
+
+def locationConfig = JenkinsLocationConfiguration.get()
+locationConfig.setAdminAddress("jenkins@tcu.gov.br")
+
+def descGitPlugin = inst.getDescriptor("hudson.plugins.git.GitSCM")
+
+descGitPlugin.setGlobalConfigName("Jenkins TCU")
+descGitPlugin.setGlobalConfigEmail("jenkins@tcu.gov.br")
+
+descGitPlugin.save()
+
 //inst.descriptor.all() - obter todos os descritores
 println "Alterando tema para material design"
 def descriptor = inst.getDescriptor(org.codefirst.SimpleThemeDecorator);
@@ -17,4 +31,7 @@ def configXml = new File('/var/jenkins_home/admin-config.xml').getText('UTF-8')
 
 def xmlStream = new ByteArrayInputStream( configXml.getBytes() )
 
-Jenkins.instance.createProjectFromXML(jobName, xmlStream)
+inst.createProjectFromXML(jobName, xmlStream)
+
+inst.save()
+inst.reload()
