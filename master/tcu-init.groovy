8f75aa2859a6390b5e9f76314cef637f4f9e6940
Dharlan Oliveira <dharlanoliveira@gmail.com>
2016-06-27 23:57:49 -0300

    * Externalizando configuração do projeto

--- master/tcu-init.groovy
+++ master/tcu-init.groovy
@@ -1,10 +1,12 @@
 import jenkins.model.*;
+import hudson.model.*;
+import com.cloudbees.hudson.plugins.folder.*
 
 def inst = Jenkins.instance;
 println "Desabilitando a segurança"
 inst.disableSecurity();
 println "Desabilitando executores no master"
-inst.setNumExecutors(0);
+inst.setNumExecutors(1);
 
 println "Configurando e-mail global"
 def config = JenkinsLocationConfiguration.get()
@@ -25,12 +27,32 @@ println "Alterando tema para material design"
 def descriptor = inst.getDescriptor(org.codefirst.SimpleThemeDecorator);
 descriptor.cssUrl = "https://jenkins-contrib-themes.github.io/jenkins-material-theme/dist/material-indigo.css"
 
-println "Criando job para uso do Job DSL Plugin"
-def jobName = "job-para-criacao-de-projetos-tcu"
-def configXml = new File('/var/jenkins_home/admin-config.xml').getText('UTF-8')
+def admin = inst.createProject(Folder,'admin');
 
-def xmlStream = new ByteArrayInputStream( configXml.getBytes() )
+println "Criando job (com Job DSL Plugin) respoansável por montar todas as builds"
+def nomeCriarJob = "criar-todos-jobs"
+def criarJobXml = new File('/var/jenkins_home/criar-todos-jobs-config.xml').getText('UTF-8')
 
-inst.createProjectFromXML(jobName, xmlStream)
+def criarJobXmlStream = new ByteArrayInputStream( criarJobXml.getBytes() )
 
+def criarJob = admin.createProjectFromXML(nomeCriarJob, criarJobXmlStream)
+
+
+println "Criando job para excluir slaves fantasmas"
+def nomeExcluirJob = "excluir-slaves-fantasmas-jobs"
+def excluirJobXml = new File('/var/jenkins_home/excluir-slaves-fantasmas-config.xml').getText('UTF-8')
+
+def excluirJobXmlStream = new ByteArrayInputStream( excluirJobXml.getBytes() )
+
+def excluirJob = admin.createProjectFromXML(nomeExcluirJob, excluirJobXmlStream)
+
+
+//inst.add(admin,'admin')
+admin.save()
+criarJob.save();
+excluirJob.save();
 inst.save()
+
+println "Forçar execução de job de criação de projetos"
+def projetoCriacao = inst.getItemByFullName('admin/criar-todos-jobs')
+inst.queue.schedule(projetoCriacao)
